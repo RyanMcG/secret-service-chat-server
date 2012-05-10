2f1e52e10a415dd8a51fc62ad7bfbbba8736920a
Ryan McGowan <ryan@ryanmcg.com>
2012-05-10 02:34:45 -0400

    Removed cruft from web.py.

--- web.py
+++ web.py
@@ -6,14 +6,9 @@ version = "0.1.0"
 
 import os
 
-from helper.forms import LoginForm, RegistrationForm, SearchForm
-from helper.user_manager import login_manager, check_login
-from functions.utils import debug_str
-from functions.config import read_system_config, init_logging
+from secret_service.config import read_system_config, init_logging
 from flask import (Flask, send_from_directory, render_template, redirect,
-        request, url_for, flash)
-from flaskext.login import (login_user, login_required, logout_user,
-        current_user)
+        request, url_for)
 from sys import argv
 
 #Create the app
@@ -22,19 +17,6 @@ app = Flask(__name__)
 #Run various initilization functions.
 read_system_config(app, argv)
 init_logging(app)
-db = init_db(app)
-mhelper = ModelHelper(app, db)
-
-#Required by Flask-Login extension
-login_manager.setup_app(app)
-
-
-#Always send the logged_in/is_admin status to a template
-@app.context_processor
-def inject_user():
-    """Tell the view whether someone is logged in or not"""
-    return {'logged_in': current_user.is_active(),
-            'is_admin': current_user.is_active() and current_user.is_admin()}
 
 
 #For browsers that do not support the link tag for favicon
@@ -48,125 +30,22 @@ def favicon():
 @app.route('/')
 def index():
     """Render the site homepage, which is also the search page."""
-    form = SearchForm()
-    search = request.args.get("search")
-    results = None
-    if search:
-        results = mhelper.get_search_results(search)
-    return render_template("index.html", form=form, results=results)
-
-
-@app.route("/logout")
-@login_required
-def logout():
-    logout_user()
-    return redirect(request.args.get("next") or url_for("index"))
-
-
-@app.route('/login', methods=["GET", "POST"])
-def login():
-    if not current_user.is_active():
-        to_page = request.args.get("next")
-        if to_page == url_for("logout"):
-            to_page = False
-        form = LoginForm()
-        if form.is_submitted():
-            if form.validate():
-                user = check_login(form.username.data, form.password.data)
-                if user:
-                    login_user(user, form.remember_me.data)
-                    flash("Logged in successfully.", "success")
-                    return redirect(to_page or url_for("index"))
-                else:
-                    flash("Invalid Username/Password", "error")
-            else:
-                for key, msg in form.errors.items():
-                    flash("Could not validate " + key + ": " + ", ".join(msg),
-                            "error")
-        return render_template("login.html", form=form)
-    else:
-        flash("You are already logged in!", "info")
-        return redirect(request.args.get("next") or url_for("index"))
+    return render_template("index.html")
 
 
 @app.route('/register', methods=["GET", "POST"])
 def register():
-    if not current_user.is_active():
-        to_page = request.args.get("next")
-        if to_page == url_for("logout"):
-            to_page = False
-        form = RegistrationForm()
-        if form.is_submitted():
-            if form.validate():
-                try:
-                    db.session.add(User(form.email.data, form.username.data,
-                        form.password.data, form.name.data, form.phone.data))
-                    db.session.commit()
-                    flash("Congratualtions, " + (form.name.data or
-                        form.username.data) + "! You have registered\
-                                successfully!", "success")
-                    return redirect(to_page or url_for("index"))
-                except:
-                    if not app.debug:
-                        flash("We're sorry, we were unable to register you.\
-                                Feel free to try again.", "error")
-                    else:
-                        raise
-            else:
-                for key, msg in form.errors.items():
-                    flash("Could not validate " + key + ": " + ", ".join(msg),
-                            "error")
-        return render_template("register.html", form=form)
-    else:
-        flash("You are already logged in!", "info")
-        return redirect(request.args.get("next") or url_for("index"))
+    return redirect(request.args.get("next") or url_for("index"))
 
 
-@app.route('/view/history')
-def view_history():
-    pass
+@app.route('/message', methods=["GET", "POST"])
+def get_message():
+    return {'herp': 'get'}
 
 
-@app.route('/view/order/<int:order_id>')
-def view_order():
-    pass
-
-
-@app.route('/view/cart')
-def view_cart():
-    pass
-
-
-@app.route('/view/profile')
-@app.route('/view/profile/<int:user_id>')
-def edit_profile():
-    users = User.get().all()
-    return debug_str(users)
-
-
-@app.route('/add/book/<int:book_id>')
-def add_book_to_cart():
-    pass
-
-
-@app.route('/create/book')
-def create_book():
-    pass
-
-
-@app.route('/view/inventory/<int:inventory_id>')
-def view_inventory():
-    pass
-
-
-@app.route('/view/inventory_order/<int:inventory_order_id>')
-def view_inventory_order():
-    pass
-
-
-@app.route('/create/inventory_order')
-def create_inventory_order():
-    pass
+@app.route('/message', methods=["PUT"])
+def put_message():
+    return {'herp': 'put'}
 
 
 def run_application():
